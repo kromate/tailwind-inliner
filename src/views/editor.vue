c45e6b475a4a123baec715b82a111ebef4c0c6ac
kromate <akpananthony33@gmail.com>
2022-07-26 03:57:32 +0100

    -

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Nav />
+  <Main />
 </template>
 
+<script setup lang="ts">
+import Nav from "@/components/Nav.vue";
+import Main from "@/components/Main.vue";
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+body {
+  --nav-height: 4.2rem;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const prefix = 'monaco-editor/esm/vs'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          jsonWorker: [`${prefix}/language/json/json.worker`],
+          cssWorker: [`${prefix}/language/css/css.worker`],
+          htmlWorker: [`${prefix}/language/html/html.worker`],
+          tsWorker: [`${prefix}/language/typescript/ts.worker`],
+          editorWorker: [`${prefix}/editor/editor.worker`]
+        }
+      }
     }
   }
 })
