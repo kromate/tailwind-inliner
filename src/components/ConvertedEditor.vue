f006e755ec377e026eb317ea3e0f36619565e02e
kromate <akpananthony33@gmail.com>
2022-07-29 15:18:57 +0100

    feat: update

--- src/components/ConvertedEditor.vue
+++ src/components/ConvertedEditor.vue
@@ -1,13 +1,33 @@
 <template>
   <div ref="container" class="absolute inset-0 h-full"></div>
+  <button
+    type="button"
+    class="rounded-full z-[5000] bg-gray-50 text-gray-500 text-xs font-semibold leading-6 py-0.5 pl-2 pr-2.5 flex items-center hover:bg-gray-100 transition-opacity select-none dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600 pointer-events-none focus:opacity-100 focus:pointer-events-auto absolute bottom-4 right-[calc(14px+0.625rem)]"
+  >
+    <svg
+      viewBox="0 0 20 20"
+      class="w-5 h-5 text-gray-400 flex-none mr-1"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      aria-hidden="true"
+    >
+      <path
+        d="M7 4.75H5.75a2 2 0 0 0-2 2v8.5a2 2 0 0 0 2 2h8.5a2 2 0 0 0 2-2v-8.5a2 2 0 0 0-2-2H13"
+      ></path>
+      <path
+        d="M12 6.25H8a1 1 0 0 1-1-1v-1.5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v1.5a1 1 0 0 1-1 1ZM7.75 10.25h4.5M7.75 13.25h4.5"
+      ></path></svg
+    ><span class="">Copy<span class="sr-only">, then focus editor</span></span>
+  </button>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, onUnmounted, watch, toRefs } from "vue";
-import { useResizeObserver, useStorage, useDebounceFn } from "@vueuse/core";
-import { initialEditorValue, StorageName, useDarkGlobal } from "../utils";
-import * as monaco from "monaco-editor";
-import { generateStyles } from "../utils/styleGenerator";
+import { onMounted, ref, onUnmounted } from "vue";
+import { useStorage } from "@vueuse/core";
+import { initialEditorValue, StorageName } from "../utils";
 import {
   mountCSSEditor,
   unmountCSSEditor,
